47524c40569be8ba224ec50296ebcd0933df4585
Deppu <39197229+Deppu@users.noreply.github.com>
2018-05-11 23:59:06 +0700

    intrinsic statistic declaration added
    Variabel maximum, minimum, mean, standard deviation, median, dan modulus dideklarasikan dalam struct_kelas.
    
    Ditambah fungsi statistic_pull yang berfungsi untuk menarik dan mengisi nilai-nilai statistic dari data kelas yang baru saja dimasukkan.
    
    Ditambah fungsi class_swap untuk merubah sesame struct_kelas dengan pointer agar dapat mencari data median, minimum, dan maximum dengan mudah.
    
    SEMUA FUNGSI DIATAS BELUM SEMPURNA!

--- PROYEK.cpp
+++ PROYEK.cpp
@@ -3,10 +3,12 @@ using namespace std;
 struct struct_mhs{
 	string nama;
 	long int nim;
-	int nilai;
+	float nilai;
 };
 struct struct_kelas{
 	int banyak_mhs;
+	float mean,std_dev,max,min,med,mod;
+	int max_address,min_address,med_address;
 	char nama;
 	struct_mhs mhs[64];
 };
@@ -16,6 +18,23 @@ struct {
 }data;
 void menu_kelas();
 void menu_tampil();
+void class_swap(struct_mhs *p,struct_mhs *q){
+	struct_mhs temp=*p;
+	*p=*q;
+	*q=temp;
+}
+void class_score_bub_sort(struct_kelas arr){
+	bool done=false;
+	while(done==false){
+		done=true;
+		for(int i=0;i<arr.banyak_mhs-1;i++){
+			if(arr.mhs[i].nilai>arr.mhs[i+1].nilai){
+				done=false;
+				class_swap(&arr.mhs[i],&arr.mhs[i+1]);
+			}
+		}
+	}
+}
 void menu(){
 	int n;
 	cout<<"1. Masukkan/Ubah Data\n2. Tampilkan Data\n";cin>>n;
@@ -23,6 +42,24 @@ void menu(){
 		case 1: menu_kelas();break;
 		case 2: menu_tampil();break;
 	}
+}
+void statistic_pull(int n){
+	struct_kelas score_sorted_temp;
+	score_sorted_temp=data.kelas[n];
+	class_score_bub_sort(score_sorted_temp);
+	data.kelas[n].max=score_sorted_temp.mhs[data.kelas[n].banyak_mhs-1].nilai;
+	data.kelas[n].min=score_sorted_temp.mhs[0].nilai;
+	data.kelas[n].med=score_sorted_temp.mhs[data.kelas[n].banyak_mhs/2].nilai;
+	int mod_counter=0;
+	for(int i=0;i<score_sorted_temp.banyak_mhs;i++){
+		cout<<"Nama\t: "<<data.kelas[n].mhs[i].nama<<endl;
+		cout<<"NIM\t: "<<data.kelas[n].mhs[i].nim<<endl;
+		cout<<"Nilai\t: "<<data.kelas[n].mhs[i].nilai<<endl;
+		cout<<endl<<endl;
+	}
+	cout<<"Max: "<<data.kelas[n].max;
+	cout<<"Min: "<<data.kelas[n].min;
+	cout<<"Med: "<<data.kelas[n].med;
 }
 void masuk_data(int n){
 	cout<<"\nBanyak Mahasiswa\t: ";cin>>data.kelas[n].banyak_mhs;cin.ignore();
@@ -32,6 +69,7 @@ void masuk_data(int n){
 		cout<<"Nilai\t: ";cin>>data.kelas[n].mhs[i].nilai;
 		cout<<endl;cin.ignore();
 	}
+	statistic_pull(n);
 }
 void tambah_kelas(){
 	cout<<"\nNama Kelas\t: ";cin>>data.kelas[data.banyak_kls].nama;
